af61b3b6b7eb1bdee4cc1167a9612149b0e0a423
Zachary Weiss <coolshale@gmail.com>
2016-11-28 22:20:22 -0500

    Zach work

--- rpc_server.rb
+++ rpc_server.rb
@@ -30,8 +30,8 @@ class RpcServer
 end
 
 def turn_on_rabbit
-	url = 'amqp://_Vjw35MM:GvzbBUIPufAYEOiKlHCyrQzjcX3wfQ3g@lean-hawkbit-1.bigwig.lshift.net:10029/p6ax6MqZ4W6t'
-	conn = Bunny.new(url, automatically_recover: false)
+	# url = 'amqp://_Vjw35MM:GvzbBUIPufAYEOiKlHCyrQzjcX3wfQ3g@lean-hawkbit-1.bigwig.lshift.net:10029/p6ax6MqZ4W6t'
+	conn = Bunny.new(ENV['RABBITMQ_BIGWIG_RX_URL'], automatically_recover: false)
 	conn.start
 	ch   = conn.create_channel
 	begin
